403bcc1a3af9b36b1dbc4043fd2c1913b329960a
criscar1998 <cristiansilva9899@gmail.com>
2024-06-05 01:18:27 -0300

    diminuiu quantidade de iceservers deixando apenas os essenciais

--- src/app/_utils/iceServers.ts
+++ src/app/_utils/iceServers.ts
@@ -1,6 +1,5 @@
 export const stunServers: RTCIceServer[] = [
     { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun.l.google.com:5349" },
     { urls: "stun:stun.cloudflare.com:3478" },
-    { urls: "stun:stunserver.stunprotocol.org:3478" }
+    { urls: "stun:stunserver.stunprotocol.org:3478"}
 ]
